6812ceb56fc8498834fc5281daecb0178715f34f
Daniel Oxman <zeoxyman@gmail.com>
2016-10-29 19:18:48 -0400

    Random enemy spawning!
    Gonna need to merge in score before we progress further...

--- WombDefender/Assets/Scripts/EnemyController.cs
+++ WombDefender/Assets/Scripts/EnemyController.cs
@@ -1,27 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TestEnemyController : MonoBehaviour {
+public class EnemyController : MonoBehaviour {
+
+	public enum EnemyType {Normal, Tough, Tricky, Potent, Boss};
 
 	private const int BASE_SPEED = 1;
 
-	public Transform target;
+	public EnemyType type;
+
+	private int hits; // How many hits this can take
 
 	// Use this for initialization
 	void Start () {
-	
+		hits = type == EnemyType.Boss ? 5 : (type == EnemyType.Tough ? 2 : 1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// Move towards womb
 		float step = BASE_SPEED * Time.deltaTime;
-		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+		transform.position = Vector3.MoveTowards (transform.position, GameObject.Find ("womb").transform.position, step);
 	}
 
 	void OnCollisionEnter2D (Collision2D coll) {
 		if (coll.gameObject.tag == "Player") { // ie. collision with the barrier
-			Destroy (gameObject);
+			hits--;
+			if (hits == 0) {
+				Destroy (gameObject);
+			} else {
+				// TODO: Behavior to bounce off shield and try again
+			}
 		}
 
 		if (coll.gameObject.name == "womb") {
