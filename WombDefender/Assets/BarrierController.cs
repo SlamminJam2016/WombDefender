708fd475e2ddd86c092c17b12732627c4d1559b1
Daniel Oxman <zeoxyman@gmail.com>
2016-10-29 14:56:38 -0400

    Thumbstick works!
    DESIGN REQUEST: Come back later and change it from free-moving thumbpad
    to a fixed track on the outside of the circle. Only if we have time
    though.

--- WombDefender/Assets/BarrierController.cs
+++ WombDefender/Assets/BarrierController.cs
@@ -5,6 +5,8 @@ public class BarrierController : MonoBehaviour {
 
 	private const int ANGLE_FACTOR = 50;
 
+	public float TargetAngle = 0; // Angle toward which we rotate with the thumbpad
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,23 +14,34 @@ public class BarrierController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// CONTROLS
-
-
 		// Keyboard controls -- for desktop only!
 		if (Input.GetKey ("left")) {
 			transform.Rotate (Vector3.back * Time.deltaTime * ANGLE_FACTOR);
+			TargetAngle = transform.eulerAngles.z; // Override the thumbpad; arrow keys are supposed to be for debug only
 		} else if (Input.GetKey ("right")) {
 			transform.Rotate (Vector3.forward * Time.deltaTime * ANGLE_FACTOR);
+			TargetAngle = transform.eulerAngles.z; // Override the thumbpad; arrow keys are supposed to be for debug only
 		}
-			
-		// TODO: Mobile controls -- send events from virtual joystick object?
 
+		// Thumbpad controls are done in ThumbpadController and affect the TargetAngle here
+		if (transform.eulerAngles.z != TargetAngle) {
+			// Figure out the more efficient direction to turn in, then rotate the barrier in that direction
+			// TODO: Do this in like 3 lines instead of 15, also label the variables better
+			float angleDifference1 = transform.eulerAngles.z - TargetAngle;
+			float angleDifference2 = -angleDifference1;
 
+			if (angleDifference1 < 0) {
+				angleDifference1 += 360;
+			}
+			if (angleDifference2 < 0) {
+				angleDifference2 += 360;
+			}
 
-		// RENDERING
-
-		// We rotate the barrier's parent object to make it turn around the womb's center
-
+			if (angleDifference1 < angleDifference2) {
+				transform.Rotate (Vector3.back * Time.deltaTime * ANGLE_FACTOR);
+			} else {
+				transform.Rotate (Vector3.forward * Time.deltaTime * ANGLE_FACTOR);
+			}
+		}
 	}
 }
