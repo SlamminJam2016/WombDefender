3e1f82c25486505c6b6bcb0abb1875f9b9050b67
Carol Yao <yaocarol96@gmail.com>
2016-10-29 14:04:57 -0400

    added HUD and womb scripts

---
+++ WombDefender/Assets/Scripts/Womb.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Womb : MonoBehaviour {
+
+	// health stats
+	public int currentHealth;
+	public int maxHealth = 5;
+
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentHealth > maxHealth) {
+			currentHealth = maxHealth;
+		} 
+
+		if(currentHealth <= 0) {
+			Die ();
+
+		}
+	
+	}
+
+	void Die() {
+		// restart game on death Note: change later to screen/tap to start
+		//SceneManager.LoadScene(SceneManager.LoadedScene);
+	}
+}
