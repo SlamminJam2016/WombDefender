736545a6fa05ae4fd3cd74129bc1fdea5b03fb6e
hiimben <ben.che9@gmail.com>
2016-10-30 10:46:49 -0400

    changed buttons at gameOver screen, added shop

---
+++ WombDefender/Assets/Scripts/retryButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class retryButton : MonoBehaviour {
+
+	public void retry(int sceneToChangeTo) {
+		
+		SceneManager.LoadScene (sceneToChangeTo);
+
+	}
+}
