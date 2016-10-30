929872d38207b18d9a9842523825ff0af778c85b
Carol Yao <yaocarol96@gmail.com>
2016-10-30 13:56:06 -0400

    Added text to items

--- WombDefender/Assets/Scripts/GameOverOptions.cs
+++ WombDefender/Assets/Scripts/GameOverOptions.cs
@@ -7,7 +7,7 @@ public class GameOverOptions : MonoBehaviour {
 	// Use this for initialization
 
 	void Start () {
-		float fadeTime = GameObject.Find ("gameOverScreen").GetComponent<Fading> ().BeginFade (1);
+		//float fadeTime = GameObject.Find ("gameOverScreen").GetComponent<Fading> ().BeginFade (1);
 	}
 
 	// Update is called once per frame
